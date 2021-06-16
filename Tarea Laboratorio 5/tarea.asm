4af473e45744e8e01ab27542c82d1b3f31036e32
aacm13 <00354817@uca.edu.sv>
2021-06-16 10:58:04 -0600

    d

---
+++ Tarea Laboratorio 5/tarea.asm
@@ -0,0 +1,47 @@
+org 100h
+
+    section .text
+    xor AX, AX
+    xor SI, SI
+    xor BX, BX
+    XOR CX, CX
+    xor DX, DX
+
+    MOV SI, 0
+    MOV DI, 0d
+    
+    call modotexto
+    call movercursor
+    call escribircaracter
+    call esperartecla
+    call exit
+
+    modotexto: 
+        MOV AH, 0h ; activa modo texto
+        MOV AL, 03h ; modo gráfico deseado, 03h denota un tamaño de 80x25 (80 columnas, 25 filas)
+        INT 10h
+        RET
+    movercursor:
+        MOV AH, 02h ; posiciona el cursor en pantalla.
+        MOV DH, 10 ;fila en la que se mostrará el cursor
+        MOV DL, 20 ;columna en la que se mostrará el cursor
+        MOV BH, 0h ; número de página
+        INT 10h
+        RET
+    escribircaracter: ;utilizando interrupcion DOS
+        MOV AH, 09h ; escribe cadena en pantalla según posición del cursor
+        MOV DX, caracter ; cadena que vamos a imprimir, debe ir sin corchetes
+        INT 21h
+        RET
+
+    esperartecla:
+        MOV AH, 00h ; espera buffer del teclado para avanzar en la siguiente instrucción
+        INT 16h
+
+        ret
+    exit:
+        int 20h
+
+section .data
+
+caracter DB "Alvaro Antonio Corpeno Molina $"
